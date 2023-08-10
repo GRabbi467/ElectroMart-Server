f4739d9b5a48563cff2d25afb3db0e16439cafc3
Golam Rabbi <57221660+GRabbi467@users.noreply.github.com>
2023-08-10 18:41:33 +0600

    search functionality added

--- index.js
+++ index.js
@@ -15,7 +15,6 @@ const port = process.env.PORT || 5000;
 app.listen(port,() => console.log(`The server is running on port ${port}`))
 
 
-
 const uri = `mongodb+srv://${process.env.DB_NAME}:${process.env.DB_PASS}@cluster0.ctntq2l.mongodb.net/?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, {
@@ -39,20 +38,59 @@ async function run() {
    app.get('/products',async(req,res)=>{
     const page = parseInt(req.query.page);
     const size = parseInt(req.query.size);
+    const search = req.query.search;
+    const query = { productName: new RegExp(search, 'i') };
+    const searchRes =await DB.find(query).toArray();
     const allProducts =await DB.find({}).skip(page*size).limit(size).toArray();
     const productsbyEmail = await DB.find({email:req.query.email}).toArray();
     const count = await DB.estimatedDocumentCount();
-   
-    res.send({allProducts,productsbyEmail,count})
+    res.send({allProducts,productsbyEmail,count,searchRes})
    });
 
-   
+  // app.get('/products', async (req, res) => {
+  //   try {
+  //     const page = parseInt(req.query.page);
+  //     const size = parseInt(req.query.size);
+  //     const search = req.query.search;
+  //     const email = req.query.email;
+  
+  //     // Creating a regular expression pattern for case-insensitive search
+  //     const query = { productName: new RegExp(search, 'i') };
+  
+  //     // Count total documents matching the search criteria
+  //     const count = await DB.countDocuments(query);
+  
+  //     // Fetch products based on the search criteria and pagination
+  //     const products = await DB.find(query)
+  //       .skip(page * size)
+  //       .limit(size)
+  //       .toArray();
+  
+  //     // Fetch products by email (assuming 'email' is a field in the collection)
+  //     const productsByEmail = await DB.find({ email }).toArray();
+  
+  //     res.json({
+  //       products,
+  //       productsByEmail,
+  //       totalCount: count,
+  //     });
+  //   } catch (error) {
+  //     console.error('Error:', error);
+  //     res.status(500).json({ error: 'Internal server error' });
+  //   }
+  // });
+  
 
    app.get('/products/:id',async(req,res)=>{
     const id = req.params.id;
     const query = {_id:new ObjectId(id)}
     const product = await DB.find(query).toArray();
     res.send(product)
+   })
+
+   app.get('/category/:category',async(req,res)=>{
+    const categorialProducts = await DB.find({category:req.query.category}).toArray();
+    res.send(categorialProducts);
    })
 
 
